6848b0589f421d0bac8ab14fc6a875995b506345
Quentin Geerts <quentin.geerts@bstorm.be>
2023-09-14 20:03:22 +0200

    Demo Event

---
+++ CounterEvent/Program.cs
@@ -0,0 +1,17 @@
+﻿using CounterEvent.Models;
+
+Counter compteur = new Counter();
+Displayer afficheur = new Displayer();
+
+// Abonnement de la méthode AfficherNouvelleValeur à l'événement ValeurChangee
+compteur.ChangedValue += afficheur.DisplayNewValue;
+
+// Incrémentation du compteur (déclenche l'événement)
+compteur.Increase(); // Affiche "Nouvelle valeur du compteur : 1"
+compteur.Increase(); // Affiche "Nouvelle valeur du compteur : 2"
+
+// Désabonnement de la méthode AfficherNouvelleValeur de l'événement
+compteur.ChangedValue -= afficheur.DisplayNewValue;
+
+// Incrémentation du compteur (l'événement n'est plus géré)
+compteur.Increase();
